613c35d717d29dc331b198f174ab81376a26d861
Leandro Silva <leandrodivinosilva12@gmail.com>
2024-10-05 15:39:35 +0000

    projeto final

--- formsmt.js
+++ formsmt.js
@@ -37,3 +37,27 @@ document.getElementById('promo-form').addEventListener('submit', function(event)
     alert("Formulário enviado com sucesso!");
   });
   
+// forge
+// URL da API
+const apiUrl ="https://forge.withub.ai/api/triggers/5257a02d-c19b-4969-ab81-2206de86082c/execute/66eebf0187e5fb740b35353c";
+
+// Dados que você quer enviar
+const data = {
+   Email: email
+};
+
+// Fazendo a requisição POST para a API
+fetch(apiUrl, {
+    method: 'POST', // Método HTTP POST
+    headers: {
+        'Content-Type': 'application/json', // Define o tipo de conteúdo como JSON
+    },
+    body: JSON.stringify(data) // Converte os dados para JSON e envia no corpo da requisição
+})
+.then(response => response.json()) // Converte a resposta da API para JSON
+.then(data => {
+    console.log('Sucesso:', data); // Exibe os dados recebidos da API
+})
+.catch(error => {
+    console.error('Erro:', error); // Captura e exibe qualquer erro que ocorrer
+});
